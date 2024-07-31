97dd8e5d93c84a6d9cf1dad128c7a6caf599e494
Fabio Napoleoni <f.napoleoni@gmail.com>
2024-07-31 17:48:05 +0200

    Team tests

---
+++ github/repo/main.tf
@@ -0,0 +1,116 @@
+terraform {
+  required_providers {
+    github = {
+      source = "integrations/github"
+    }
+  }
+}
+
+variable "name" {
+  description = "The name of the repository"
+  type        = string
+}
+
+variable "description" {
+  description = "The description of the repository"
+  type        = string
+  default     = null
+}
+
+variable "homepage_url" {
+  description = "The homepage of the repository"
+  type        = string
+  default     = null
+}
+
+variable "has_issues" {
+  description = "Whether the repository has issues enabled"
+  type        = bool
+  default     = true
+}
+
+variable "has_projects" {
+  description = "Whether the repository has issues enabled"
+  type        = bool
+  default     = true
+}
+
+variable "has_dependabot" {
+  description = "Whether the repository has issues enabled"
+  type        = bool
+  default     = false
+}
+
+variable "secrets" {
+  type      = map(string)
+  default   = {}
+  sensitive = true
+}
+
+variable "digitalocean_deploy" {
+  description = "Whether the repository could deploy using DigitalOcean"
+  type        = bool
+  default     = false
+}
+
+variable "shared_workflows" {
+  description = "Whether the repository shares workflows"
+  type        = bool
+  default     = false
+}
+
+variable "teams" {
+  description = "The teams to add to the repository"
+  type        = map(string)
+  default     = {}
+}
+
+resource "github_repository" "repo" {
+  name                        = var.name
+  description                 = var.description
+  homepage_url                = var.homepage_url
+  visibility                  = "private"
+  allow_merge_commit          = false
+  allow_rebase_merge          = false
+  allow_update_branch         = true
+  delete_branch_on_merge      = true
+  has_downloads               = false
+  has_issues                  = var.has_issues
+  has_projects                = var.has_projects
+  squash_merge_commit_message = "BLANK"
+  squash_merge_commit_title   = "PR_TITLE"
+  vulnerability_alerts        = var.has_dependabot
+}
+
+# Configure a secret for each passed value
+resource "github_actions_secret" "secrets" {
+  for_each        = nonsensitive(var.secrets)
+  repository      = github_repository.repo.name
+  secret_name     = each.key
+  plaintext_value = each.value
+}
+
+# Expose workflow files to other repositories in our organization
+# @see https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/enabling-features-for-your-repository/managing-github-actions-settings-for-a-repository#managing-access-for-a-private-repository-in-an-organization
+resource "github_actions_repository_access_level" "shared_workflow_access" {
+  count        = var.shared_workflows ? 1 : 0
+  access_level = "organization"
+  repository   = github_repository.repo.name
+}
+
+# Manage team permissions if specified
+data "github_team" "team" {
+  for_each = var.teams
+  slug     = each.key
+}
+
+resource "github_team_repository" "team_permissions" {
+  for_each   = var.teams
+  team_id    = data.github_team.team[each.key].id
+  repository = github_repository.repo.name
+  permission = each.value
+}
+
+output "repository" {
+  value = github_repository.repo
+}
