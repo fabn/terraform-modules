80429fcf49e39a2d9679e56636d8dabf7e7dac5b
Fabio Napoleoni <f.napoleoni@gmail.com>
2025-01-26 08:56:24 +0100

    Remove check

--- k8s/rancher/main.tf
+++ k8s/rancher/main.tf
@@ -11,16 +11,3 @@ terraform {
     }
   }
 }
-
-# Temporary check used as precondition in the module to ensure we're on kind
-data "kubernetes_namespace_v1" "ns" {
-  metadata {
-    name = "local-path-storage"
-  }
-  lifecycle {
-    postcondition {
-      condition     = "local-path-storage" == self.metadata.0.name
-      error_message = "Not ready for non kind clusters"
-    }
-  }
-}
