20d823c53ad16988fbeaeedd5198663aba8cf07a
Fabio Napoleoni <f.napoleoni@gmail.com>
2025-03-28 10:28:24 +0100

    Add namespace output

--- k8s/datadog/outputs.tf
+++ k8s/datadog/outputs.tf
@@ -2,3 +2,8 @@ output "chart_version" {
   description = "Installed chart version of the datadog operator chart"
   value       = one(helm_release.datadog_operator.metadata).version
 }
+
+output "namespace" {
+  description = "The namespace where the datadog operator is deployed"
+  value       = helm_release.datadog_operator.namespace
+}
